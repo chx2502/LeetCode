2db899a1a728dc8bf3e9594cf2ab8b69fedf5be2
chx <chx2502@gmail.com>
2020-07-23 22:31:30 +0800

    2020.07.23

--- LeetCode_64_最小路径和/LeetCode_64_最小路径和/main.cpp
+++ LeetCode_64_最小路径和/LeetCode_64_最小路径和/main.cpp
@@ -16,23 +16,39 @@ public:
     /*
      动态规划，参考官方题解4，在原数组上从终点出发更新各结点的路经长
      **/
+//    int minPathSum(vector<vector<int>>& grid) {
+//        const int m = (int)grid.size();
+//        if (m == 0) return 0;
+//        const int n = (int)grid[0].size();
+//        int i, j;
+//        i = m;
+//        j = n-1;
+//        while (i-- > 0) {
+//            while (j-- > 0) {
+//                if (i < m-1 && j < n-1)
+//                    grid[i][j] += grid[i][j+1] < grid[i+1][j] ? grid[i][j+1] : grid[i+1][j];
+//                else if (i < m-1)
+//                    grid[i][j] += grid[i+1][j];
+//                else
+//                    grid[i][j] += grid[i][j+1];
+//            }
+//            j = n;
+//        }
+//        return grid[0][0];
+//    }
     int minPathSum(vector<vector<int>>& grid) {
-        const int m = (int)grid.size();
+        int m = (int)grid.size();
         if (m == 0) return 0;
-        const int n = (int)grid[0].size();
-        int i, j;
-        i = m;
-        j = n-1;
-        while (i-- > 0) {
-            while (j-- > 0) {
-                if (i < m-1 && j < n-1)
-                    grid[i][j] += grid[i][j+1] < grid[i+1][j] ? grid[i][j+1] : grid[i+1][j];
-                else if (i < m-1)
-                    grid[i][j] += grid[i+1][j];
-                else
-                    grid[i][j] += grid[i][j+1];
+        int n = (int)grid[0].size();
+        if (n == 0) return 0;
+        
+        for (int row = m-1; row >=0; row--) {
+            for (int col = n-1; col >= 0; col--) {
+                if (row < m-1 && col < n-1) grid[row][col] += min(grid[row+1][col], grid[row][col+1]);
+                else if (row < m-1) grid[row][col] += grid[row+1][col];
+                else if (col < n-1) grid[row][col] += grid[row][col+1];
+                else continue;
             }
-            j = n;
         }
         return grid[0][0];
     }
